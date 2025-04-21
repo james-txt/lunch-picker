46c481cd0a9d760cc1a7c38354046afc7926e2cb
james-txt <jameslo.txt@gmail.com>
2025-04-20 20:08:12 -0400

    feat: integrate Vercel Analytics for enhanced tracking and performance insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
